d8494f0229d77c003e5617e7e22aeb726ccba18d
崔峰博 <617990031@qq.com>
2024-12-12 11:07:37 +0800

    typefix

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -81,14 +81,14 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-    try {
-        await sql`DELETE FROM invoices WHERE id = ${id}`;
-        revalidatePath('/dashboard/invoices');
-        return { message: 'Deleted Invoice.' };
-    } catch (error) {
-        return { message: 'Database Error: Failed to Delete Invoice.' };
-    }
-
+    // try {
+    //     await sql`DELETE FROM invoices WHERE id = ${id}`;
+    //     revalidatePath('/dashboard/invoices');
+    //     return { message: 'Deleted Invoice.' };
+    // } catch (error) {
+    //     return { message: 'Database Error: Failed to Delete Invoice.' };
+    // }
+    return { message: 'Database Error: Failed to Delete Invoice.' };
 }
 
 export async function authenticate(
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -19,10 +19,10 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const updateInvoiceWithId = ;
   
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={()=>{updateInvoice.bind(null, invoice.id)}}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
